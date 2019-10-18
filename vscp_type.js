167d407cca08da24f43ba1b2f7422c8d4dd8eb2e
Ake Hedman, Grodans Paradis AB <akhe@grodansparadis.com>
2019-10-18 09:14:18 +0200

    Correction in spec by troky

--- vscp_type.js
+++ vscp_type.js
@@ -39,7 +39,7 @@
             !!!!!!!!!!!!!!!!!!!!  W A R N I N G  !!!!!!!!!!!!!!!!!!!!
                            This file is auto-generated
                 see https://github.com/grodansparadis/vscp-classes
-                        Generated: 2019-10-15 11:26:30.291742
+                        Generated: 2019-10-18 09:09:31.491295
 */
 
 
