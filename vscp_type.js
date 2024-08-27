46543ad1073c59bc4757336480f7c72f272b2255
Ake Hedman <akhe@grodansparadis.com>
2024-08-27 16:22:31 +0200

    Updated type defs

--- vscp_type.js
+++ vscp_type.js
@@ -39,7 +39,7 @@
             !!!!!!!!!!!!!!!!!!!!  W A R N I N G  !!!!!!!!!!!!!!!!!!!!
                            This file is auto-generated
                 see https://github.com/grodansparadis/vscp-classes
-                        Generated: 2023-04-19 20:30:59.619149
+                        Generated: 2024-04-08 08:39:26.226518
 */
 
 'use strict';
